e4cd3c4f05fd723f8f02abb0640ef2af9c262028
VISHESH TRIPATHI <tripathiishu467@gmail.com>
2024-06-28 11:08:11 +0530

    Update Auth.js

--- src/appwrite/Auth.js
+++ src/appwrite/Auth.js
@@ -55,7 +55,7 @@ export class AuthService{
     }
     async getCurrentUser(){
         try{
-            return this.account.get();
+            return  await this.account.get();
         }catch(error){
             console.log("user information not found");
         }
